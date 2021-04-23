db39ceba72dc50bc90748074e1b7d8333448924c
wangkui0508 <wangkui0508@hotmail.com>
2021-04-23 15:35:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# -march=armv8-a+crypto 
-gcc -c -O3 -msse4.1 -msha *.c
+test `uname -p ` == aarch64 || gcc -c -O3 -msse4.1 -msha *.c
+test `uname -p ` == aarch64 && gcc -c -O3 -march=armv8-a+crypto *.c
 ar -crs libsha256.a *.o
 
